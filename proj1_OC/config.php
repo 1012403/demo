68fc1ef4853865994f6aeade7e36cc9820b710c2
Daisy <hoathachthao1606@gmail.com>
2016-06-22 17:23:14 +0700

    Create Project
    Signed-off-by: Daisy <hoathachthao1606@gmail.com>

---
+++ proj1_OC/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/demo/proj1_OC/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/demo/proj1_OC/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\demo\proj1_OC/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\demo\proj1_OC/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\demo\proj1_OC/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\demo\proj1_OC/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\demo\proj1_OC/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\demo\proj1_OC/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\demo\proj1_OC/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\demo\proj1_OC/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\demo\proj1_OC/system/upload/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\demo\proj1_OC/system/modification/');
+define('DIR_LOGS', 'C:\xampp\htdocs\demo\proj1_OC/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'proj1_OC');
+define('DB_PREFIX', 'oc_');
