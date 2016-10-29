f30600d7323924564e467f68b1012eeee57cc8a3
Daisy <hoathachthao1606@gmail.com>
2016-10-29 12:48:00 +0700

    create new project
    Signed-off-by: Daisy <hoathachthao1606@gmail.com>

---
+++ noithat/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://noithat/admin/');
+define('HTTP_CATALOG', 'http://noithat/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://noithat/admin/');
+define('HTTPS_CATALOG', 'http://noithat/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/demo/noithat/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/demo/noithat/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/demo/noithat/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/demo/noithat/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/demo/noithat/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/demo/noithat/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/demo/noithat/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/demo/noithat/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/demo/noithat/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/demo/noithat/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/demo/noithat/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/demo/noithat/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'noithat');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'noithat_');
---
+++ noithat/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://noithat/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://noithat/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/demo/noithat/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/demo/noithat/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/demo/noithat/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/demo/noithat/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/demo/noithat/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/demo/noithat/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/demo/noithat/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/demo/noithat/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/demo/noithat/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/demo/noithat/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/demo/noithat/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'noithat');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'noithat_');
